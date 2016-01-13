6b9a362ba4d349d29b3274b7d6e1d06b71e633c7
jtabach <jeff.n.tabachnick@gmail.com>
2016-01-12 23:59:19 -0800

    append transactions to list

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 
 $(document).ready(init);
 
+var tNum = 001;
+var trans;
+var date;
+var amount;
+
 function init() {
-	
+	$('form').submit(getInput);
+}
+
+function getInput(e) {
+	e.preventDefault();
+
+	trans = $('input[type="text"]').val();
+	date = $('input[type="date"]').val();
+	amount = $('input[type="number"]').val();
+	cloneRow();
+}
+
+function cloneRow() {
+	var $tr = $('#template').clone();
+	$tr.removeAttr('id');
+	$tr.children('.tNum').text(tNum);
+	$tr.children('.descr').text(trans);
+	$tr.children('.date').text(date);
+	$tr.children('.deposit').text(amount);
+	$('#transactions').append($tr);
+	tNum++;
 }
