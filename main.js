93d4397eb810e9f3f18864e4b0e9c3a98598041f
jtabach <jeff.n.tabachnick@gmail.com>
2016-01-13 02:38:10 -0800

    adjustable account balance

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 $(document).ready(init);
 
 var tNum = 001;
-var accountBalance = 1000;
+var accountBalance = [1000];
 
 function init() {
 	$('form').submit(getInput);
@@ -23,16 +23,19 @@ function cloneRow(amountType) {
 	$tr.children('.descr').text($('input[type="text"]').val());
 	var datePreFormat = $('input[type="date"]').val();
 	$tr.children('.date').text(moment(datePreFormat).format('LL'));
-
+	var currency = Number($('input[type="number"]').val());
 	if (amountType === "Deposit") {
-		$tr.children('.deposit').text($('input[type="number"]').val())
+		$tr.children('.deposit').text(currency.toFixed(2))
 			.addClass('green');
+		accountBalance.push(currency);
 	} else {
-		$tr.children('.withdrawal').text($('input[type="number"]').val())
+		$tr.children('.withdrawal').text(currency.toFixed(2))
 			.addClass('red');
+		accountBalance.push(currency * -1);
 	}
 	$('#transactions').append($tr);
 	tNum++;
+	updateBalance();
 }
 
 function setDeposit() {
@@ -45,5 +48,20 @@ function setDeposit() {
 }
 
 function removeTransaction() {
-	$(this).closest('tr').remove();
+	var removed = $(this).closest('tr').remove();
+	accountBalance.splice(removed.find('.tNum').text(), 1, 0);
+	updateBalance()
 }
+
+function updateBalance() {
+	var myBalance = accountBalance.reduce(function(sum, num) {
+		return sum + num;
+	}).toFixed(2);
+	// console.log(myBalance);
+	console.log(accountBalance)
+	$('#accBalance').text(myBalance);
+}
+
+
+
+
